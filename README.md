30233d2b1fa9cd7cae9cbbc31536c47b7078198d
이현민 <smurf@kakao.com>
2024-11-13 20:49:50 +0900

    readme : 11-13 contents added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ### using Open Weather API, make a weather dashboard that will run in the browser and feature dynamically updated via react
+
+### 11-13
+
+1.  ![alt text](public/readme1.png)
+    ![alt text](public/readme2.png)
+2.  ![alt text](public/readme3.png)
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,13 @@
 import SearchBar from '@/components/layout/Header/SearchBar';
 import { CloudSunRain } from 'lucide-react';
+import Logo from '@/assets/images/Logo.svg?react';
 
 const Header = () => {
   return (
     <>
       <header className="mb-2 flex items-center gap-4">
         <h1 className="flex items-center gap-2 font-black text-2xl text-white">
-          <CloudSunRain strokeWidth={3} />
+          <Logo width={48} height={48} />
           Weather.io
         </h1>
         <SearchBar />
